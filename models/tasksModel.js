3cd6427fc0ed82d861f094ed79db38b26a8f9850
kenshi <samuduchanuka.com>
2024-04-08 20:14:06 +0530

    Change datetime(UTC) requested to correspond to local datetime

--- models/tasksModel.js
+++ models/tasksModel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const { DateTime, Zone } = require('luxon');
 
 const Schema = mongoose.Schema;
 
 const tasksSchema = new Schema({
-    title: { type:String, required: true },
+    title: { type:String}   , //required: true },
     description: String,
     dueDate: Date,
     priority: {
@@ -18,6 +19,22 @@ const tasksSchema = new Schema({
     categoryID: Schema.Types.ObjectId,
     createdAt: { type: Date, default: new Date()},
     updatedAt: Date,
+});
+
+tasksSchema.virtual('date_created_formatted').get(function() {
+    return DateTime.fromJSDate(this.createdAt).toLocaleString(DateTime.DATETIME_MED);
+});
+
+tasksSchema.virtual('due_date_formatted').get(function() {
+    if (this.dueDate){
+        return DateTime.fromJSDate(this.dueDate).toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY);
+    }
+});
+
+tasksSchema.virtual('date_updated_formatted').get(function() {
+    if (this.updatedAt){
+        return DateTime.fromJSDate(this.updatedAt).toLocaleString(DateTime.DATETIME_MED);
+    }
 });
 
 module.exports = mongoose.model("Tasks", tasksSchema);
