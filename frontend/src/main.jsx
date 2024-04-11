5453661ec7c4517622c0098bdfe001d01d06d3bd
kenshi <samuduchanuka.com>
2024-04-11 13:07:46 +0530

    Change routing from BrowserRouter to createBrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.jsx';
+import Home from './Home.jsx';
+import TaskForm from './TaskForm.jsx'
+import CategoryForm from './CategoryForm.jsx';
+import Error from './Error.jsx';
+
 // import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        path: "/new-task",
+        element: <TaskForm />,
+      },
+      {
+        path: "/new-category",
+        element: <CategoryForm />,
+      },
+    ]
+  },
+  {
+    path: "*",
+    element: <Error />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
