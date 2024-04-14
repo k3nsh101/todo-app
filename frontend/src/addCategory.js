6b5c3be2f1e5e47297c5fc9a0ef60a3f6e1e940e
kenshi <samuduchanuka.com>
2024-04-14 11:21:07 +0530

    Intall and use axios

--- frontend/src/addCategory.js
+++ frontend/src/addCategory.js
@@ -1,17 +1,14 @@
+import axios from 'axios';
+
 const addCategory = async function(category, description) {
-    const res = await fetch(
-        'http://localhost:3000/category/',
+    const res =  await axios.post(
+        "http://localhost:3000/category/",
         {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                title: category,
-                description
-            }),
+            title: category,
+            description
         }
-    );
+    )
+
     return res;
 };
 
