452acec0ec07d302f40e32861a7cea257a3c52db
kenshi <samuduchanuka.com>
2024-04-15 23:12:10 +0530

    Add the popup detailed view for the selected task

---
+++ frontend/src/useTask.js
@@ -0,0 +1,33 @@
+// Get the specific task
+
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function useTask(taskId) {
+    const [taskData, setTaskData] = useState({
+        title: "",
+        dueData: "",
+        categoryID: {
+            title: "",
+            description: ""
+        },
+        priority: "",
+        description: "",
+        status: "Pending",
+    });
+
+    useEffect(() => {
+        getTask();
+
+        async function getTask() {
+            const res = await axios.get(
+                `http://localhost:3000/tasks/${taskId}`
+            );
+            
+            const tasks = await res.data;
+            setTaskData(tasks);
+        }
+    }, [taskId]);
+
+    return taskData;
+}
