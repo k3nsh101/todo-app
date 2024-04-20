5a1c575a10814fe6677e516e88bc03a4c6d573cf
kenshi <samuduchanuka.com>
2024-04-21 01:40:46 +0530

    Add user authentication

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,35 @@
 require('dotenv').config( {path: './config/.env'} );
+require("./strategies/localStrategy");
 
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const session = require("express-session");
+const passport = require("passport");
 
 const port = process.env.PORT || 3000;
 const mongoDB = process.env.DATABASE_URL;
 
 const tasksRouter = require('./routes/tasks');
-const indexRouter = require('./routes/index');
 const categoryRouter = require('./routes/category');
 const userRouter = require('./routes/users');
+const authRouter = require("./routes/auth");
 
 const app = express();
+app.use(cors({credentials: true, origin: "http://localhost:5173"}));
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.use(session({secret: "cats", resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/public', express.static('public'))
-app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/', indexRouter);
 app.use('/tasks', tasksRouter);
 app.use('/category', categoryRouter);
 app.use('/users', userRouter);
+app.use("/auth", authRouter);
 
 app.listen(port, function() {
     console.log(`App listening on the port http://localhost:${port}/`);
