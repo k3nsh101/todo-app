77c46088518661e622b85892d1bf77bf6b01c956
kenshi <samuduchanuka.com>
2024-04-21 01:38:05 +0530

    Add logout button

--- frontend/src/SharedLayout.jsx
+++ frontend/src/SharedLayout.jsx
@@ -1,25 +1,45 @@
 // navigation bar
 import { Link, Outlet } from "react-router-dom";
-import { Box} from "@mui/material";
+import { Box, Button } from "@mui/material";
+
+import logout from "./logout";
 
 import logo from "/logo.webp";
 
+import "./sharedlayout.css";
+
 export default function SharedLayout() {
+    const handleLogout = async () => {
+        try {
+            const res = await logout();
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+        }
+
+    };
+
     return(
         <>
             <header>
-                <Link to="/">
-                    <Box
-                        className="heading-logo"
-                        component="img"
-                        sx={{
-                            maxHeight: { xs: 233, md: 150 },
-                            maxWidth: { xs: 350, md: 250 },
-                        }}
-                        alt="Homepage Logo."
-                        src={logo}
-                    />
-                </Link>
+                <div className="container">
+                    <Link to="/">
+                        <Box
+                            className="heading-logo"
+                            component="img"
+                            sx={{
+                                maxHeight: { xs: 233, md: 150 },
+                                maxWidth: { xs: 350, md: 250 },
+                            }}
+                            alt="Homepage Logo."
+                            src={logo}
+                        />
+                    </Link>
+                    <div className="settings">
+                        <Button className="logout" onClick={handleLogout}>Logout</Button>
+                    </div>
+                </div>
+
                 <nav>
                     {/* navigation bar */}
                 </nav>
