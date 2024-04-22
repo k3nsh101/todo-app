676bc40ea56ad8cdd35ccfdb1e48c905a9b05b89
kenshi <samuduchanuka.com>
2024-04-22 16:58:29 +0530

    Rename function names

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -3,19 +3,19 @@ const router = express.Router();
 
 const tasksController = require('../controllers/tasksController');
 
-// Retrieve all tasks
-router.get('/', tasksController.tasks_list);
+// Retrieve tasks by the user
+router.get('/', tasksController.getPendingTasks);
 
 // Create new task
 router.post('/', tasksController.create_task);
 
 // Retrieve the specific task
-router.get('/:taskid', tasksController.get_task);
+router.get('/:taskId', tasksController.get_task);
 
 // Update the specific task
-router.put('/:taskid', tasksController.task_update);
+router.put('/:taskId', tasksController.task_update);
 
 // Delete the specific task
-router.delete('/:taskid', tasksController.task_delete);
+router.delete('/:taskId', tasksController.task_delete);
 
 module.exports = router;
